f8129f1917bd4b0dcbe62f90f3b8647729df2798
TariqAHassan <tariqaahassan@gmail.com>
2018-03-28 14:27:53 -0700

    Update structure and namespace

--- R/artist.R
+++ R/artist.R
@@ -1,14 +1,12 @@
-get_field <- function(field) {
-  if (is.null(field)) return(NaN)
-  return(field)
-}
+source("R/utils.R")
 
+#' @export
 get_artist <- function(artist_id){
   r <- GET(glue("api.genius.com/artists/{artist_id}"),
            add_headers("Accept" = "application/json", "Host" = "api.genius.com",
                        "Authorization" = glue("Bearer {access_token}")))
   artist <- content(r, "parsed")
-  
+
   res <- data.frame("id" = artist_id,
                     "alternate_name" = get_field(artist$response$artist$alternate_names[[1]]),
                     "facebook_name" = get_field(artist$response$artist$facebook_name),
@@ -18,5 +16,3 @@ get_artist <- function(artist_id){
   )
   return(res)
 }
-
-
--- R/utils.R
+++ R/utils.R
@@ -1,11 +1,18 @@
 utf8_to_html <- function(string){
-  #' 
   #'
   #'
   #'
-  out <- stri_enc_toutf8(string) %>% 
-    url_encode() %>% 
+  #'
+  out <- stri_enc_toutf8(string) %>%
+    url_encode() %>%
     toupper()
-  
+
   return(out)
+}
+
+
+#' @keywords internal
+get_field <- function(field) {
+  if (is.null(field)) return(NaN)
+  return(field)
 }
