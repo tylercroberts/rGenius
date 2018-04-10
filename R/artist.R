0ed9cd12d29419ce130f6fe41499d48ce2d284a1
TariqAHassan <tariqaahassan@gmail.com>
2018-04-10 04:12:57 -0700

    Heavily refactor and simplify codebase

--- R/artist.R
+++ R/artist.R
@@ -1,18 +1,24 @@
+# Artists
+
 source("R/utils.R")
 
-#' @export
-get_artist <- function(artist_id. access_token){
-  r <- GET(glue("api.genius.com/artists/{artist_id}"),
-           add_headers("Accept" = "application/json", "Host" = "api.genius.com",
-                       "Authorization" = glue("Bearer {access_token}")))
-  artist <- content(r, "parsed")
 
-  res <- data.frame("id" = artist_id,
-                    "alternate_name" = get_field(artist$response$artist$alternate_names[[1]]),
-                    "facebook_name" = get_field(artist$response$artist$facebook_name),
-                    "instagram_name" = get_field(artist$response$artist$instagram_name),
-                    "twitter_name" = get_field(artist$response$artist$twitter_name),
-                    "description" = get_field(artist$response$artist$description$dom$children[[1]][[2]][[1]])
-  )
-  return(res)
+#' @export
+get_artist <- function(artist_id, access_token) {
+    #' Get Artist.
+    #'
+    #' @param artist_id :
+    #' @param access_token :
+    #'
+    artist <- .get_payload(url=glue("api.genius.com/artists/{artist_id}"))
+
+    res <- data.frame(
+      "id" = artist_id,
+      "alternate_name" = get_field(artist$response$artist$alternate_names[[1]]),
+      "facebook_name" = get_field(artist$response$artist$facebook_name),
+      "instagram_name" = get_field(artist$response$artist$instagram_name),
+      "twitter_name" = get_field(artist$response$artist$twitter_name),
+      "description" = get_field(artist$response$artist$description$dom$children[[1]][[2]][[1]])
+    )
+    return(res)
 }
