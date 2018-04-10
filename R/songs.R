5bac8401e8f4c38d4eac5453149bed20b8b48505
TariqAHassan <tariqaahassan@gmail.com>
2018-04-10 04:20:43 -0700

    Fix Typo

--- R/songs.R
+++ R/songs.R
@@ -83,7 +83,7 @@ get_songs <- function(song_ids, access_token, verbose=TRUE) {
 #' @keywords internal
 .extract_ids <- function(songs){
     #' Harvest song ids from the yeild of
-    #' `content(r, "parsed")`, where R is a GET response.
+    #' `content(r, "parsed")`, where r is a GET response.
     ids <- lapply(songs$response$hits, function(x) x$result$id)
     return(ids)
 }
