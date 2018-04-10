a3bcf0382e6e5deb571288d609618285c61786bb
TariqAHassan <tariqaahassan@gmail.com>
2018-04-10 04:13:13 -0700

    Functioning tests

--- tests/testthat/test_get_song.R
+++ tests/testthat/test_get_song.R
@@ -1,14 +1,27 @@
-library(dplyr)
-library(glue)
-library(httr)
-library(rGenius)
+context("songs.R")
 
 
-test_that("get_song() returns a list", {
-  expect_match(class(get_song(10, access_token)), "data.frame")
+source("../../api_key.R")
+
+N <- 10
+
+
+test_that(
+  "get_song() returns a list", {
+    expect_match(
+      class(get_song(N, access_token)),
+      "data.frame"
+    )
 })
 
-test_that("get_songs() output is the same length as song_ids"{
-  song_ids = seq(1:10)
-  expect_match(length(get_songs(song_ids, access_token)), length(song_ids))
+
+test_that(
+  "get_songs() output is the same length as song_ids", {
+    song_ids <- seq(1, N)
+    expected <- length(song_ids)
+    actual <- length(get_songs(song_ids, access_token))
+    expect_equal(actual, expected)
 })
+
+
+# ToDo: search_song()
