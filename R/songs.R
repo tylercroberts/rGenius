a58c8c5c0a8591e2e44b1be33d24e586d517e4b4
TariqAHassan <tariqaahassan@gmail.com>
2018-04-14 21:33:29 -0700

    Add roxygen2 documentation to song.R

--- R/songs.R
+++ R/songs.R
@@ -3,15 +3,35 @@
 source("R/utils.R")
 
 
+#' Get songs.
+#'
+#' @description This function returns information from the Genius API
+#' about a single song.
+#'
+#' @param song_id a song's ID on the Genius API
+#'
+#' @param access_token the user's (your) access token to the Genius API.
+#' For more detail about how to get it, see section Details
+#'
+#' @return A DataFrame with information about a song. Columns:
+#'
+#' \describe{
+#'   \item{id}{the song's ID on the Genius API}
+#'   \item{title}{the song's title}
+#'   \item{artist}{the song's main artist}
+#'   \item{featured_artists}{a list of other artists who contributed to the song, if any}
+#'   \item{album}{the song's album}
+#'   \item{date}{the song's released date}
+#'   \item{views}{the number of views for this song on the Genius website}
+#'   \item{contributors}{the number of contibutors to the lyrics of this songs on the Genius website}
+#'   \item{transcribers}{the number of transcribers to the lyrics of this songs on the Genius website}
+#'   \item{concurrents}{the number of people looking at the page whenever the API was last updated}
+#' }
+#'
 #' @export
 get_song <- function(song_id, access_token) {
-    #' Get songs.
-    #'
-    #' @param song_id :
-    #' @param access_token :
-    #'
     song <- .get_payload(
-      url=glue("api.genius.com/songs/{song_id}"),
+      url=glue::glue("api.genius.com/songs/{song_id}"),
       access_token=access_token
     )
 
@@ -19,6 +39,8 @@ get_song <- function(song_id, access_token) {
       "id" = song_id,
       "title" = get_field(song$response$song$title),
       "artist" = get_field(song$response$song$album$artist$name),
+      # Black magic to add the list in as a
+      # single column properly. `I()` treats the object 'as-is'.
       "featured_artists" = I(
         list(
           get_field(
@@ -27,8 +49,6 @@ get_song <- function(song_id, access_token) {
             )
           )
       ),
-      # Black magic to add the list in as a
-      # single column properly. I() treats the object "as-is".
       "album" = get_field(song$response$song$album$name),
       "date" = get_field(song$response$song$release_date),
       "views" = get_field(song$response$song$stats$pageviews),
@@ -39,23 +59,34 @@ get_song <- function(song_id, access_token) {
     return(res)
 }
 
-
+#' Bulk Song Harvesting engine.
+#'
+#' @description Wrap \code{get_song()} to automate song harvesting. Internal
+#' Function.
+#'
+#' @param res a res (result) DataFrame. Can be empty (i.e., will be populated here).
+#'
+#' @param itera song ids to iterate over and harvest
+#'
+#' @param access_token the user's (your) access token to the Genius API.
+#' For more detail about how to get it, see section Details
+#'
+#' @param verbose If TRUE, then a progression bar of the execution of the
+#' function is displayed on the console.
+#'
+#' @return a DataFrame populated with the output of \code{get_song()}
+#' for all songs in itera.
+#'
+#' @keywords internal
 .fetch_engine <- function(res, itera, access_token, verbose){
-    #' Wrap `get_song()` to automate song harvesting.
-    #'
-    #' @param res :
-    #' @param itera :
-    #' @param access_token :
-    #' @param verbose :
-    #'
     if (verbose){
-        pb <- txtProgressBar(min = 0, max = length(itera), initial = 0)
+        pb <- utils::txtProgressBar(min=0, max=length(itera), initial=0)
     }
     c <- 1
     for (i in itera) {
-        res <- rbind(res, get_song(i, access_token))
+        res <- rbind(res, get_song(song_id=i, access_token=access_token))
         if (verbose){
-            setTxtProgressBar(pb, c)
+            utils::setTxtProgressBar(pb, c)
             c <- c + 1
         }
     }
@@ -63,14 +94,38 @@ get_song <- function(song_id, access_token) {
 }
 
 
+#' Get multiple songs.
+#'
+#' @description harvest information on multiple songs.
+#'
+#' @param res a res (result) DataFrame. Can be empty (i.e., will be populated here).
+#'
+#' @param itera song ids to iterate over and harvest
+#'
+#' @param access_token the user's (your) access token to the Genius API.
+#' For more detail about how to get it, see section Details
+#'
+#' @param verbose If TRUE, then a progression bar of the execution of the
+#' function is displayed on the console.
+#'
+#' @return a DataFrame populated with the output of \code{get_song()}
+#' for all songs in song_ids. Columns:
+#'
+#' \describe{
+#'   \item{id}{the song's ID on the Genius API}
+#'   \item{title}{the song's title}
+#'   \item{artist}{the song's main artist}
+#'   \item{featured_artists}{a list of other artists who contributed to the song, if any}
+#'   \item{album}{the song's album}
+#'   \item{date}{the song's released date}
+#'   \item{views}{the number of views for this song on the Genius website}
+#'   \item{contributors}{the number of contibutors to the lyrics of this songs on the Genius website}
+#'   \item{transcribers}{the number of transcribers to the lyrics of this songs on the Genius website}
+#'   \item{concurrents}{the number of people looking at the page whenever the API was last updated}
+#' }
+#'
 #' @export
 get_songs <- function(song_ids, access_token, verbose=TRUE) {
-    #' Get songs.
-    #'
-    #' @param song_ids
-    #' @param access_token
-    #' @param verbose
-    #'
     # Expected columns: id, title, artist, featured_artists
     # album, date, views, contributors, transcribers, concurrents.
     res <- .fetch_engine(
@@ -81,46 +136,86 @@ get_songs <- function(song_ids, access_token, verbose=TRUE) {
 }
 
 
-#' @keywords internal
-.extract_ids <- function(songs){
-    #' Harvest song ids from the yield of
-    #' `content(r, "parsed")`, where r is a GET response.
-    ids <- lapply(songs$response$hits, function(x) x$result$id)
-    return(ids)
-}
-
-
+#' Get results from the API for a given URL.
+#'
+#' @description Get results from the API for some
+#' query (search?q=) URL. Internal Function.
+#'
+#' @param url an 'api.genius.com' url
+#'
+#' @param access_token the user's (your) access token to the Genius API.
+#' For more detail about how to get it, see section Details
+#'
+#' @param verbose If TRUE, then a progression bar of the execution of the
+#' function is displayed on the console.
+#'
+#' @return A DataFrame. Columns:
+#'
+#' \describe{
+#'   \item{id}{the song's ID on the Genius API}
+#'   \item{title}{the song's title}
+#'   \item{artist}{the song's main artist}
+#'   \item{featured_artists}{a list of other artists who contributed to the song, if any}
+#'   \item{album}{the song's album}
+#'   \item{date}{the song's released date}
+#'   \item{views}{the number of views for this song on the Genius website}
+#'   \item{contributors}{the number of contibutors to the lyrics of this songs on the Genius website}
+#'   \item{transcribers}{the number of transcribers to the lyrics of this songs on the Genius website}
+#'   \item{concurrents}{the number of people looking at the page whenever the API was last updated}
+#' }
+#'
 #' @keywords internal
 .song_get <- function(url, access_token, verbose){
-    #'
-    #'
-    #' @param url :
-    #' @param access_token :
-    #' @param verbose :
-    #'
+    # Get the query results form Genius' servers.
     songs <- .get_payload(url=url, access_token=access_token)
-    # Expected columns: song_id, title, artist_id, artist.
+    # Harvest song ids from `song`.
+    ids <- lapply(songs$response$hits, function(x) x$result$id)
+    # Fetch information about those song ids.
     res <- .fetch_engine(
-        res=data.frame(), itera=.extract_ids(songs),
+        res=data.frame(), itera=ids,
         access_token=access_token, verbose=verbose
     )
     return(res)
 }
 
 
+#' Search and download results for a song.
+#'
+#' @description Obtain nformation from the Genius API about an artist.
+#' There can be several songs returned if the name appears in several
+#' songs' title in the API.
+#'
+#' @param song_title the title of a song (will be searched for)
+#'
+#' @param access_token the user's (your) access token to the Genius API.
+#' For more detail about how to get it, see section Details
+#'
+#' @param n_per_page the maximum number of songs that shoud be fetch by the API.
+#' Defaults to 20.
+#'
+#' @param verbose If TRUE, then a progression bar of the execution of the
+#' function is displayed on the console.
+#'
+#' @return A DataFrame. Columns:
+#'
+#' \describe{
+#'   \item{id}{the song's ID on the Genius API}
+#'   \item{title}{the song's title}
+#'   \item{artist}{the song's main artist}
+#'   \item{featured_artists}{a list of other artists who contributed to the song, if any}
+#'   \item{album}{the song's album}
+#'   \item{date}{the song's released date}
+#'   \item{views}{the number of views for this song on the Genius website}
+#'   \item{contributors}{the number of contibutors to the lyrics of this songs on the Genius website}
+#'   \item{transcribers}{the number of transcribers to the lyrics of this songs on the Genius website}
+#'   \item{concurrents}{the number of people looking at the page whenever the API was last updated}
+#' }
+#'
 #' @export
 search_song <- function(song_title, access_token,
                         n_per_page=20, verbose=FALSE) {
-    #' Search for a song (`song_title`).
-    #'
-    #' @param song_title :
-    #' @param access_token :
-    #' @param n_per_page :
-    #' @param verbose :
-    #'
-    # Expected columns: song_id, title, artist_id, artist.
     res <- .song_get(
-        url=glue("api.genius.com/search?q={song_title}&per_page={n_per_page}"),
+        url=glue::glue("api.genius.com/search?q={song_title}&per_page={n_per_page}"),
         access_token=access_token,
         verbose=verbose
     )
@@ -128,20 +223,42 @@ search_song <- function(song_title, access_token,
 }
 
 
+#' Get songs for an artist.
+#'
+#' @description This function returns information from the Genius API
+#' about the top songs from an artist, given this artist name.
+#'
+#' @param artist_name the name of an artist (will be searched for)
+#'
+#' @param access_token the user's (your) access token to the Genius API.
+#' For more detail about how to get it, see section Details
+#'
+#' @param n_per_page the maximum number of songs that shoud be fetch by the API.
+#' Defaults to 20.
+#'
+#' @param verbose If TRUE, then a progression bar of the execution of the
+#' function is displayed on the console.
+#'
+#' @return A DataFrame. Columns:
+#'
+#' \describe{
+#'   \item{id}{the song's ID on the Genius API}
+#'   \item{title}{the song's title}
+#'   \item{artist}{the song's main artist}
+#'   \item{featured_artists}{a list of other artists who contributed to the song, if any}
+#'   \item{album}{the song's album}
+#'   \item{date}{the song's released date}
+#'   \item{views}{the number of views for this song on the Genius website}
+#'   \item{contributors}{the number of contibutors to the lyrics of this songs on the Genius website}
+#'   \item{transcribers}{the number of transcribers to the lyrics of this songs on the Genius website}
+#'   \item{concurrents}{the number of people looking at the page whenever the API was last updated}
+#' }
+#'
 #' @export
-get_song_from_artists <- function(artist_name,
-                                  access_token,
-                                  n_per_page=20,
-                                  verbose=FALSE) {
-    #' Get songs for an individual Artist.
-    #'
-    #' @param artist_name :
-    #' @param access_token :
-    #' @param n_per_page :
-    #' @param verbose :
-    #'
+get_song_from_artists <- function(artist_name, access_token,
+                                  n_per_page=20, verbose=FALSE){
     res <- .song_get(
-        url=glue("api.genius.com/search?q={artist_name}&per_page={n_per_page}"),
+        url=glue::glue("api.genius.com/search?q={artist_name}&per_page={n_per_page}"),
         access_token=access_token,
         verbose=verbose
     )
