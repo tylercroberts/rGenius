a8d2626e4ef6b882b4ad6b2d792196e1a047d70b
TariqAHassan <tariqaahassan@gmail.com>
2018-04-15 04:24:17 -0700

    Add test for `R/utils#get_field()`

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -1,5 +1,16 @@
+# Utils Test
+
 context("utils.R")
 
 test_that("utf8_to_html() returns the correct string", {
-  expect_match(utf8_to_html("éèàÀ"), "%C3%A9%C3%A8%C3%A0%C3%80")
+    expect_match(utf8_to_html("éèàÀ"), "%C3%A9%C3%A8%C3%A0%C3%80")
 })
+
+
+test_that("get_field() returns NaN when passed NULL", {
+    expect_true(is.nan(get_field(NULL)))
+})
+
+
+# Note: `.get_payload()` put through its paces
+# the functions in `R/artist` and `R/songs.R`.
